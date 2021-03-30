bda22176a3d8c9c7ace190fe69823079f0ea2d49
Ramon Meffert <ramon.meffert@gmail.com>
2021-03-30 11:50:27 +0200

    Add profile validation and comments

--- game.py
+++ game.py
@@ -7,9 +7,17 @@ class GameType(Enum):
 class Game:
 
     def __init__(self, game_type, agenda, quota, profile):
+        # The game type, either AMENDMENT or SUCCESSIVE.
         self.game_type = game_type
+        
+        # The initial agenda.
         self.agenda    = agenda
+        
+        # The quota, i.e. the number of votes that an alternative has to reach
+        # in order for it to win.
         self.quota     = quota
+
+        # A profile of voter ballots
         self.profile   = profile
 
     def outcome(self):
@@ -19,25 +27,45 @@ class Game:
             return self.outcome_successive(self.agenda)
 
     def outcome_amendment(self, agenda):
+        """Recursive algorithm for calculating the outcome of the amendment
+        procedure for a given agenda. Recursive calls receive a copy of the
+        agenda with either the first or second alternative removed, whichever 
+        did not reach the quota or was excluded due to tie-breaking.
+
+        Args:
+            agenda ([type]): [description]
+
+        Returns:
+            [type]: [description]
+        """
 
         if len(agenda) >= 3:
-            # agenda minus option x2
+            # If there are at least 3 items, we can modify the agenda and do a
+            # recursive call
+
+            # agenda minus option 2, i.e. [x1, x3, ..., xm]
             new_agenda_left = [agenda[0]] + agenda[2:]
-            # agenda minus option x1
+            # agenda minus option 1, i.e. [x2, x3, ..., xm]
             new_agenda_right = agenda[1:]
 
+            # calculate outcomes for both agendas
             outcome_1_3 = self.outcome_amendment(new_agenda_left)
             outcome_2_3 = self.outcome_amendment(new_agenda_right)
 
+            # condition: o^A(x1, x3, …, xm) P o^A(x2, x3, …, xm)
             num_prefers_left = self.profile.num_prefers(outcome_1_3, outcome_2_3)
             
-            
+            # Pick an outcome with tie breaking: if the left branch doesn't win,
+            # take the right branch.
             if num_prefers_left >= self.quota:
                 return outcome_1_3
             else:
                 return outcome_2_3
 
         elif len(agenda) == 2:
+            # If there are only two items left, pick the one that reaches the
+            # quota (again with tie-breaking)
+
             num_prefers_left = self.profile.num_prefers(agenda[0], agenda[1])
 
             if num_prefers_left >= self.quota:
--- profile.py
+++ profile.py
@@ -1,14 +1,22 @@
 import csv
+from typing import Set
+
 
 class Profile:
     def __init__(self):
-        
+
+        # Register a CSV dialect. Is this the right place to do this?
+        # No clue. But I want to be sure my dialect exists so this at least
+        # ensures that I guess.
         csv.register_dialect("ignore_whitespace", delimiter=",",
-                     skipinitialspace=True, quoting=csv.QUOTE_MINIMAL)
+                             skipinitialspace=True, quoting=csv.QUOTE_MINIMAL)
 
         # A ballot is a key-value pair where the key is the voter id and the
         # value is an ordered list of alternatives
         self.ballots = {}
+
+        # Save the available alternatives as a set
+        self.alternatives = set()
 
     @staticmethod
     def from_csv(path: str) -> 'Profile':
@@ -40,6 +48,9 @@ class Profile:
                 for voter in reader.fieldnames:
                     new_profile.ballots[int(voter)].append(line[voter])
 
+        # This will raise an error if the profile is invalid
+        new_profile.__validate()
+
         return new_profile
 
     @staticmethod
@@ -69,9 +80,61 @@ class Profile:
                 preference = split[1].split()
                 new_profile.ballots[voter_id] = preference
 
+        # This will raise an error if the profile is invalid
+        new_profile.__validate()
+
         return new_profile
 
+    def __validate(self):
+        """Validates the profile by:
+
+        * Checking the length of all ballots is the same
+        * Checking if the set of alternatives is the same for all ballots
+        """
+
+        # Retrieve the first ballot and take the alternatives in there as the
+        # available alternatives
+        first_voter_id = next(iter(self.ballots))
+
+        # update(list) on a Set adds all items in that list to the set
+        self.alternatives.update(self.ballots.get(first_voter_id))
+
+        # iterate over voter preferences
+        # (self.ballots.values() is a list of lists of alternatives)
+        for voter, preferences in self.ballots.items():
+            # if a ballot has a different number of alternatives than are
+            # available, or if it contains alternatives not in the set of
+            # alternatives, throw an error
+            length_different = len(preferences) != len(self.alternatives)
+            alternatives_differ = set(
+                preferences).difference(self.alternatives)
+
+            # Provide helpful error message
+            if len(alternatives_differ) != 0:
+                if length_different:
+                    # Different length _and_ different composition
+                    raise ValueError(f"\n\tVoter {voter} has a different set "
+                                     "of alternatives than I was expecting. I was expecting "
+                                     f"{len(self.alternatives)} alternatives but got "
+                                     f"{len(preferences)} instead.\n\tAlso, these alternatives "
+                                     f"do not appear in the first ballot: {alternatives_differ}.")
+                else:
+                    # Only different composition
+                    raise ValueError(f"\n\tVoter {voter} has a different set "
+                                     "of alternatives than I was expecting. These alternatives "
+                                     f"do not appear in the first ballot: {alternatives_differ}.")
+            elif length_different:
+                raise ValueError(f"\n\tVoter {voter} has a different set "
+                                 "of alternatives than I was expecting. I was expecting "
+                                 f"{len(self.alternatives)} alternatives but got "
+                                 f"{len(preferences)} instead.")
+
     def ballot(self, id):
+        """Returns the ballot of a voter with id `id`.
+
+        Returns:
+            [type]: [description]
+        """
         return self.ballots[id]
 
     def prefers(self, id, a1, a2) -> bool:
@@ -84,6 +147,9 @@ class Profile:
         return pref.index(a1) < pref.index(a2)
 
     def num_prefers(self, a1, a2):
+        """The number of agents that prefer alternative `a1` over `a2`.
+
+        """
         num = 0
 
         for voter in self.ballots.keys():
