47935c3fb7c970c8b6e098c3337fe1418751895c
Ramon Meffert <ramon.meffert@gmail.com>
2021-04-07 23:28:15 +0200

    Add multiprocessing to speed up analysis

--- analysis.py
+++ analysis.py
@@ -1,11 +1,14 @@
-from itertools import permutations
+import itertools
+import math
+import concurrent.futures
 from gametype import GameType
 from profile import Profile
 from game import Game
+from multiprocessing import Manager
+
 
 class Analysis:
-    
-    def __init__(self, type, profile, quota, expected_outcome = None):
+    def __init__(self, type, profile, quota, expected_outcome=None):
         self.type = type
         self.profile = profile
         self.quota = quota
@@ -21,21 +24,38 @@ class Analysis:
 
         outcomes = dict.fromkeys(self.profile.alternatives, 0)
 
-        for permutation in permutations(self.profile.alternatives):
-            game = Game(self.type, list(permutation), self.quota, self.profile)
-            outcome = game.outcome()
-            # uncomment the line below for debugging
-            # print(f"agenda = {list(permutation)}, outcome = {outcome}")
-            outcomes[outcome] += 1
+        permutations = itertools.permutations(self.profile.alternatives)
+        total = math.factorial(len(self.profile.alternatives))
 
-        nonzero_outcomes = list(filter(lambda x : x[1] > 0, outcomes.items()))
+        outcomes_temp = []
 
-        outcome = sorted(list(map(lambda tup : self.profile.alternative_name(tup[0]), nonzero_outcomes)))
+        with concurrent.futures.ProcessPoolExecutor() as executor:
+            for permutation in permutations:
+                outcomes_temp.append(
+                    executor.submit(calculate_outcome, self.type, permutation, self.quota, self.profile)
+                )
+
+        for outcome in outcomes_temp:
+            result = outcome.result()
+            outcomes[result] = outcomes.get(result) + 1
+
+        nonzero_outcomes = list(filter(lambda x: x[1] > 0, outcomes.items()))
+
+        outcome = sorted(
+            list(
+                map(lambda tup: self.profile.alternative_name(tup[0]), nonzero_outcomes)
+            )
+        )
+
+        percentage = 0
 
         if self.expected_outcome != None:
             total = sum(outcomes.values())
             num_expected_outcome = outcomes[self.expected_outcome]
             percentage = num_expected_outcome / total * 100
-            print(f"The expected outcome, {self.profile.alternative_name(self.expected_outcome)}, occured {round(percentage, 1)}% ({num_expected_outcome}/{total}) of the time.")
 
-        return outcome
+        return percentage, outcome
+
+def calculate_outcome(type, permutation, quota, profile):
+    game = Game(type, list(permutation), quota, profile)
+    return game.outcome()
